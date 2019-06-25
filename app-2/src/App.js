d11c659161e31e12e7c1e1d287df411cc5fa2b9c
detanaded <48308236+detanaded@users.noreply.github.com>
2019-06-25 08:35:46 -0600

    App-2 complete
    Added the ability to display foods  on the dom.

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react'
 
-class App extends Component {
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      foods:[
+        'chicken', 'bbq', 'ice cream', 'fries', 'turkey'
+      ]
+    }
+  }
   render() {
+    let displayFoods= this.state.foods.map((element, index)=>{
+      return <h2 key={index}>{element}</h2>
+    })
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {displayFoods}
       </div>
-    );
+    )
   }
 }
-
-export default App;
