47c0397813ae59a3c3033e8e645b9703df81a57e
detanaded <48308236+detanaded@users.noreply.github.com>
2019-06-24 20:57:18 -0600

    App-1 done
    Created input that displays text as you type

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react'
 
-class App extends Component {
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      message: ''
+    }
+  }
+
+  handleChange(value) {
+    this.setState({message: value})
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input onChange={e => this.handleChange(e.target.value)} type="text"/>
+        <p>{this.state.message}</p>
       </div>
-    );
+    )
   }
 }
-
-export default App;
